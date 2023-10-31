8e66272bb78d081be6b1e506e9fa2ed08eb8f550
Aguiar Gustavo <gaguiar@andreani.com>
2023-10-31 12:01:06 -0300

    validations added

--- src/componentes/PermissionManager.js
+++ src/componentes/PermissionManager.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+
 import React, { useState, useEffect } from "react";
 import {
   Table,
@@ -9,15 +11,19 @@ import {
   Paper,
   Button,
   TextField,
-  Grid
+  Grid,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+  Snackbar,
+  Alert
 } from "@mui/material";
 import { makeStyles } from "@mui/styles"
 import axios from "axios";
+import { customTheme } from "./themes/customTheme";
 
-// Import your customized theme
-import { customTheme } from "./themes/customTheme"; // Replace with the path to your theme file
-
-const API_URL = "https://localhost:5001/api/permissions"; // Replace with your API endpoint
+const API_URL = "https://localhost:5001/api/permissions";
 
 let api = axios.create({
   baseURL: API_URL,
@@ -41,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
   form: {
     display: "flex",
     alignItems: "center",
-    marginBottom: 2,
+    margin: 2,
   },
   input: {
     marginRight: 2,
@@ -49,12 +55,26 @@ const useStyles = makeStyles((theme) => ({
   tableContainer: {
     marginTop: 3,
   },
+  error: {
+    borderColor: "red !important",
+  },
 }));
 
 function PermissionManager() {
   const [permissions, setPermissions] = useState([]);
   const [newPermission, setNewPermission] = useState({});
   const [selectedPermission, setSelectedPermission] = useState(null);
+  const [selectedPermissionType, setSelectedPermissionType] = useState("");
+  const [selectedPermissionTypeModif, setSelectedPermissionTypeModif] = useState("");
+  const [errors, setErrors] = useState({});
+  const [errorsModif, setErrorsModif] = useState({});
+  const [snackbar, setSnackbar] = useState({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+    message: ''
+  });
+
   const classes = useStyles();
 
   useEffect(() => {
@@ -66,55 +86,187 @@ function PermissionManager() {
     });
   }, []);
 
-  const handleCreatePermission = () => {
-    // Send a POST request to create a new permission
-    api.post(API_URL, { permission: newPermission }).then((response) => {
-      setPermissions([...permissions, response.data.data]);
-      setNewPermission({});
+  const permissionTypes = [
+    {
+      id: 1,
+      description: 'Admin'
+    },
+    {
+      id: 2,
+      description: 'Employee'
+    }
+  ];
+
+  const validatePermission = (validations, permission, prop) => {
+    let errors = {
+      permissionTypeId: 'You must select a permission type',
+      employeeName: 'Employee name cannot be empty',
+      employeeLastName: 'Employee last name cannot be empty'
+    }
+
+    if (prop) {
+      if (!permission[prop]) {
+        validations[prop] = errors[prop];
+      } else {
+        validations[prop] = null;
+      }
+
+      return validations;
+    }
+
+    if (!permission.permissionTypeId) {
+      validations.permissionTypeId = errors.permissionTypeId;
+    }
+
+    if (!permission.employeeName) {
+      validations.employeeName = errors.employeeName;
+    }
+
+    if (!permission.employeeLastName) {
+      validations.employeeLastName = errors.employeeLastName;
+    }
+  
+    return validations;
+  }
+
+  const setSnackbarError = (message) => {
+    setSnackbar({
+      open: true,
+      vertical: 'top',
+      horizontal: 'center',
+      severity: 'error',
+      message: message
     });
+  }
+
+  const setSnackbarSuccess = (message) => {
+    setSnackbar({
+      open: true,
+      vertical: 'top',
+      horizontal: 'center',
+      severity: 'success',
+      message: message
+    });
+  }
+
+  const handleCreatePermission = () => {
+    newPermission.permissionTypeId = selectedPermissionType;
+
+    let validations = validatePermission({}, newPermission);
+
+    if (Object.keys(validations).length === 0) {
+      api.post(API_URL, { permission: newPermission }).then((response) => {
+        let newItem = {...response.data.data, permissionType: permissionTypes.find(x => x.id == newPermission.permissionTypeId)};
+        setPermissions([newItem].concat(permissions));
+        setNewPermission({});
+        setSelectedPermissionType(""); 
+        setSnackbarSuccess('Entity created successfuly');
+      });
+    } else {
+      setErrors(validations);
+      setSnackbarError('The fields have some errors');
+    }
+  };
+
+  const handleCancelUpdatePermission = () => {
+    setSelectedPermission(null);
   };
 
   const handleUpdatePermission = () => {
-    // Send a PUT request to update the selected permission
-    api.put(`${API_URL}/${selectedPermission.id}`, { permission: selectedPermission }).then(() => {
-      setPermissions(permissions.map((p) => (p.id === selectedPermission.id ? selectedPermission : p)));
-      setSelectedPermission(null);
-    });
+    selectedPermission.permissionTypeId = selectedPermissionTypeModif;
+
+    let validations = validatePermission({}, selectedPermission);
+
+    if (Object.keys(validations).length === 0) {
+      api.put(`${API_URL}/${selectedPermission.id}`, { permission: selectedPermission }).then(() => {
+        setPermissions(permissions.map((p) => (p.id === selectedPermission.id ? selectedPermission : p)));
+        setSelectedPermission(null);
+        setSnackbarSuccess('Entity updated successfuly');
+      });
+    } else {
+      setErrorsModif(validations);
+      setSnackbarError('The fields have some errors');
+    }
   };
 
   const handleDeletePermission = (id) => {
-    // Send a DELETE request to delete a permission
     api.delete(`${API_URL}/${id}`).then(() => {
       setPermissions(permissions.filter((permission) => permission.id !== id));
     });
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbar(prev => { return {...prev, open: false }});
+  };
+
+  const { vertical, horizontal } = snackbar;
+
   return (
   <div className={classes.root}>
+    <Snackbar 
+      open={snackbar.open} 
+      autoHideDuration={6000} 
+      onClose={handleClose}
+      anchorOrigin={{ vertical, horizontal }}
+      key={vertical + horizontal}
+      >
+      <Alert onClose={handleClose} severity={snackbar.severity} sx={{ width: '100%' }}>
+        {snackbar.message}
+      </Alert>
+    </Snackbar>
     <h1>Permission Manager</h1>
     <Grid container spacing={2} className={classes.form}>
       <Grid item>
-        <TextField
-          className={classes.input}
-          label="Permission Type Id"
-          value={newPermission.permissionTypeId || ""}
-          onChange={(e) => setNewPermission({ ...newPermission, permissionTypeId: e.target.value })}
-        />
+        <FormControl variant="outlined">
+          <InputLabel id="dropdown-label" className={`${classes.input} ${errors.permissionTypeId && 'boder-color-red'}`}>Permission Type</InputLabel>
+          <Select
+            className={`${classes.input} ${errors.permissionTypeId && 'boder-color-red'}`}
+            labelId="dropdown-label"
+            id="dropdown"
+            value={selectedPermissionType}
+            onChange={(e) => {
+              let permissionToCreate = { ...newPermission, permissionTypeId: e.target.value };
+              setNewPermission(permissionToCreate);
+              setSelectedPermissionType(e.target.value);
+              setErrors(validatePermission({...errors}, permissionToCreate, 'permissionTypeId'));
+            }}
+            label="Select an option"
+            style={{minWidth: 200}}
+          >
+            {permissionTypes.map(x => (
+              <MenuItem key={x.id} value={x.id}>{x.description}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item>
+        <FormControl variant="outlined">
+          <TextField
+            className={`${classes.input} ${errors.employeeName && 'boder-color-red'}`}
+            label="Employee Name"
+            value={newPermission.employeeName || ""}
+            onChange={(e) => {
+              let permissionToCreate = { ...newPermission, employeeName: e.target.value };
+              setNewPermission(permissionToCreate);
+              setErrors(validatePermission({...errors}, permissionToCreate, 'employeeName'));
+            }}
+          />
+        </FormControl>
       </Grid>
       <Grid item>
         <TextField
-          className={classes.input}
-          label="Employee Name"
-          value={newPermission.employeeName || ""}
-          onChange={(e) => setNewPermission({ ...newPermission, employeeName: e.target.value })}
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          className={classes.input}
+          className={`${classes.input} ${errors.employeeLastName && 'boder-color-red'}`}
           label="Employee Last Name"
           value={newPermission.employeeLastName || ""}
-          onChange={(e) => setNewPermission({ ...newPermission, employeeLastName: e.target.value })}
+          onChange={(e) => {
+            let permissionToCreate = { ...newPermission, employeeLastName: e.target.value };
+            setNewPermission(permissionToCreate);
+            setErrors(validatePermission({...errors}, permissionToCreate, 'employeeLastName'));
+          }}
         />
       </Grid>
       <Grid item>
@@ -123,44 +275,79 @@ function PermissionManager() {
         </Button>
       </Grid>
     </Grid>
+    { Object.keys(errors).length > 0 && <Grid className={'boder-color-red'}>{ 
+      Object.values(errors).map((x, i) => (
+        <>{x && <li key={x}>{x}</li>}</>
+    ))}</Grid>}
     {selectedPermission && (
-      <Grid container spacing={2} className={classes.form}>
-        <Grid item>
-          <TextField
-            className={classes.input}
-            label="Permission Type Id"
-            value={selectedPermission.permissionTypeId || ""}
-            onChange={(e) => setSelectedPermission({ ...selectedPermission, permissionTypeId: e.target.value })}
-          />
+      <>
+        <Grid container spacing={2} className={classes.form} style={{marginTop: 10}}>
+          <Grid item>
+            <FormControl variant="outlined">
+              <InputLabel id="dropdown-label" className={`${classes.input} ${errorsModif.permissionTypeId && 'boder-color-red'}`}>Permission Type</InputLabel>
+              <Select
+                className={`${classes.input} ${errorsModif.permissionTypeId && 'boder-color-red'}`}
+                labelId="dropdown-label"
+                id="dropdown"
+                value={selectedPermissionTypeModif}
+                onChange={(e) => {
+                  let newSelectedPermission = { ...selectedPermission, permissionTypeId: e.target.value };
+                  setSelectedPermission(newSelectedPermission);
+                  setSelectedPermissionTypeModif(e.target.value);
+                  setErrorsModif(validatePermission({...errorsModif}, newSelectedPermission, 'permissionTypeId'));
+                }}
+                label="Select an option"
+                style={{minWidth: 200}}
+              >
+                <MenuItem value="1">Admin</MenuItem>
+                <MenuItem value="2">Employee</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <TextField
+              className={`${classes.input} ${errorsModif.employeeName && 'boder-color-red'}`}
+              label="Employee Name"
+              value={selectedPermission.employeeName || ""}
+              onChange={(e) => {
+                let newSelectedPermission = { ...selectedPermission, employeeName: e.target.value };
+                setSelectedPermission(newSelectedPermission);
+                setErrorsModif(validatePermission({...errorsModif}, newSelectedPermission, 'employeeName'));
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              className={`${classes.input} ${errorsModif.employeeLastName && 'boder-color-red'}`}
+              label="Employee Last Name"
+              value={selectedPermission.employeeLastName || ""}
+              onChange={(e) => {
+                let newSelectedPermission = { ...selectedPermission, employeeLastName: e.target.value };
+                setSelectedPermission(newSelectedPermission);
+                setErrorsModif(validatePermission({...errorsModif}, newSelectedPermission, 'employeeLastName'));
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={handleUpdatePermission}>
+              Update
+            </Button>
+            <Button variant="contained" color="secondary" onClick={handleCancelUpdatePermission}>
+              Cancel
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField
-            className={classes.input}
-            label="Employee Name"
-            value={selectedPermission.employeeName || ""}
-            onChange={(e) => setSelectedPermission({ ...selectedPermission, employeeName: e.target.value })}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            className={classes.input}
-            label="Employee Last Name"
-            value={selectedPermission.employeeLastName || ""}
-            onChange={(e) => setSelectedPermission({ ...selectedPermission, employeeLastName: e.target.value })}
-          />
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={handleUpdatePermission}>
-            Update
-          </Button>
-        </Grid>
-      </Grid>
+        { Object.keys(errorsModif).length > 0 && <Grid className={'boder-color-red'}>{ 
+          Object.values(errorsModif).map(x => (
+            <>{x && <li key={x}>{x}</li>}</>
+        ))}</Grid>}
+      </>
     )}
-    <TableContainer component={Paper} className={classes.tableContainer}>
+    <TableContainer component={Paper} className={classes.tableContainer} style={{marginTop: 20}}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Permission Type Id</TableCell>
+            <TableCell>Permission Type</TableCell>
             <TableCell>Employee Name</TableCell>
             <TableCell>Employee Last Name</TableCell>
             <TableCell>Action</TableCell>
@@ -169,21 +356,25 @@ function PermissionManager() {
         <TableBody>
           {permissions.map((permission) => (
             <TableRow key={permission.id}>
-              <TableCell>{permission.permissionTypeId}</TableCell>
+              <TableCell>{permission.permissionType.description}</TableCell>
               <TableCell>{permission.employeeName}</TableCell>
               <TableCell>{permission.employeeLastName}</TableCell>
               <TableCell>
-                <Button
+                {/* <Button
                   variant="contained"
                   color="secondary"
                   onClick={() => handleDeletePermission(permission.id)}
                 >
                   Delete
-                </Button>
+                </Button> */}
                 <Button
                   variant="contained"
                   color="primary"
-                  onClick={() => setSelectedPermission(permission)}
+                  onClick={() => {
+                    setSelectedPermission(permission);
+                    setSelectedPermissionTypeModif(permission.permissionTypeId);
+                    setErrorsModif({});
+                  }}
                 >
                   Edit
                 </Button>
